7e8bf71732c89d80d06da5c359e584bf0b999c67
1030283726 <1030283726@qq.com>
2025-08-25 23:14:32 +1000

    Add onChange={handleChange}

--- src/views/Options/Setting.js
+++ src/views/Options/Setting.js
@@ -239,7 +239,7 @@ export default function Settings() {
             name="fabClickAction"
             value={fabClickAction}
             label={i18n("fab_click_behavior")}
-            onChange={(e) => updateFab({ fabClickAction: e.target.value })}
+            onChange={handleChange}
           >
             <MenuItem value={0}>{i18n("fab_click_menu")}</MenuItem>
             <MenuItem value={1}>{i18n("fab_click_translate")}</MenuItem>
