c2bcca0e45003cb7d9aa168809b245cc6129950a
1030283726 <1030283726@qq.com>
2025-08-26 22:20:17 +1000

    Change to onChange={handleChange}

--- src/views/Options/Setting.js
+++ src/views/Options/Setting.js
@@ -79,7 +79,7 @@ export default function Settings() {
         value = limitNumber(value, 0, 4);
         break;
         case "fabClickAction":
-        value = limitNumber(value,0,1);
+        value = limitNumber(value, 0, 1);
        break;
       case "contextMenuType":
         isExt && sendBgMsg(MSG_CONTEXT_MENUS, value);
@@ -243,11 +243,7 @@ export default function Settings() {
             name="fabClickAction"
             value={fabClickAction}
             label={i18n("fab_click_behavior")}
-            onChange={(e) =>
-              updateSetting({
-                fabClickAction: Number(e.target.value),
-              })
-            }
+            onChange={handleChange}
           >
             <MenuItem value={0}>{i18n("fab_click_menu")}</MenuItem>
             <MenuItem value={1}>{i18n("fab_click_translate")}</MenuItem>
